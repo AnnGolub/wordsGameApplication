aa55f1a590615b74351226882df8dfab2547f271
Sergey-Kosichkin <d978827@icloud.com>
2021-04-18 12:41:21 +0300

    Test commit

---
+++ wordsGameApplication/wordsGameApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordsGameApplication
+//
+//  Created by Сергей Косичкин on 18.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+// изменения
+}
+
